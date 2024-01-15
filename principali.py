9596978db1b7a6631a42ba433b56aca9a9b3d423
yourYoungBear <beardevelopments12.com>
2024-01-15 08:21:53 -0800

    Struttura principali variabili e metodi basici

---
+++ principali.py
@@ -0,0 +1,24 @@
+# Variabili
+
+stringa = "ciao"
+numero = 12
+numero_decimale = 1.2
+booleano = True
+carattere = "c"
+
+''' In python non bisogna indicare la tipologia del dato per far si che il codice funzioni, basta intestarle
+ un nome e un valore '''
+
+# Principali funzioni
+print()
+input()
+type()
+
+'''
+
+print --> Permette di stampare una stringa 
+input --> Prelevare un valore
+type --> Verificare la tipologia della variabile (es. type(numero) --> ci restituira che e un int)
+
+'''
+
--- testpy.py
+++
@@ -1,1 +0,0 @@
-print("Ciao bellissimo")
